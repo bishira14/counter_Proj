95ba458bbe92da3eec3d33eed4b847a4ad45db16
Hamza Bishira <bishira12@hotmail.co.uk>
2020-02-24 19:10:33 +0000

    latest update

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
- 
+ let countLabel = document.querySelector('#count');
+ let lowerCounter = document.querySelector('#lowerCount');
+ let addCounter = document.querySelector('#addCount');
+
+ let counter = countLabel.innerHTML;
+
+ addCounter.addEventListener('click', function(){
+   counter++;
+   countLabel.innerHTML = counter;
+   if(counter < 0){
+     countLabel.style.color = 'red';
+   }else if(counter === 0){
+     countLabel.style.color = 'black';
+   }else {
+     countLabel.style.color = 'green';
+   }
+
+ })
+
+ lowerCounter.addEventListener('click', function(){
+  counter--;
+  countLabel.innerHTML = counter;
+  if(counter < 0){
+    countLabel.style.color = 'red';
+  }else if(counter === 0){
+    countLabel.style.color = 'black';
+  }else {
+    countLabel.style.color = 'green';
+  }
+
+ })
